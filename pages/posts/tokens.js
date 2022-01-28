72bf48133c6fb9b980f9d9ae7ff7907c3bf6c954
shreyjain13 <shreyd.jain@mail.utoronto.ca>
2022-01-28 11:50:10 -0500

    First commit

---
+++ pages/posts/tokens.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Tokenomic Optimization</h1>
+      <h2>
+        <Link href="/">
+          <a>Home page</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
